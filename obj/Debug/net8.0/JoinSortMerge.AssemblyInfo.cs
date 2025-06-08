b784d36a8935cd421f73a927c69325856eaa2f3a
Ruan-H <ruandavidoliveira003@gmail.com>
2025-06-08 19:46:09 -0300

    Remove bin e obj

--- obj/Debug/net8.0/JoinSortMerge.AssemblyInfo.cs
+++ obj/Debug/net8.0/JoinSortMerge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JoinSortMerge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc0026c54d956c44c7e3482b989fe98231e808a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e187682a73f28168d7a6773fab2720a7e1dea87")]
 [assembly: System.Reflection.AssemblyProductAttribute("JoinSortMerge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JoinSortMerge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
